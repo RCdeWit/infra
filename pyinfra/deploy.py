e94350a8bf10828344132d10a63cdbf89802ecde
RCdeWit <RCdeWit@users.noreply.github.com>
2024-03-21 11:27:22 +0100

    Configure Tailscale on VPS

--- pyinfra/deploy.py
+++ pyinfra/deploy.py
@@ -1,27 +1,42 @@
 import os
 
-from pathlib import Path
+from pyinfra import host
+from pyinfra.facts.server import LinuxDistribution
 from pyinfra.operations import apt, files, server, systemd
 
+linux_distribution = host.get_fact(LinuxDistribution)
+linux_name = linux_distribution['release_meta']['ID']
+linux_codename = linux_distribution['release_meta']['CODENAME']
+
 apt.key(
-    _sudo=True,
     name="Add the Caddy apt gpg key",
     src="https://dl.cloudsmith.io/public/caddy/stable/gpg.key",
+    _sudo=True,
 )
 
-# apt.key(
-#     name="Add the Tailscale apt gpg key",
-#     src="https://download.docker.com/linux/ubuntu/gpg",
-# )
+apt.key(
+    name='Add tailscale apt gpg key',
+    src='https://pkgs.tailscale.com/stable/{}/{}.noarmor.gpg'.format(linux_name, linux_codename),
+    _sudo=True
+)
 
 apt.repo(
+    name="Add repo for Caddy",
+    src="deb https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main",
     _sudo=True,
-    name="Source for Caddy",
-    src="deb https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main"
+)
+
+apt.repo(
+    name="Add repo for Tailscale",
+    src='deb https://pkgs.tailscale.com/stable/{} {} main'.format(linux_name, linux_codename),
+    filename='tailscale',
+    _sudo=True,
 )
 
 apt.packages(
-    packages=["caddy"],
+    packages=["caddy", "tailscale"],
+    update=True,
+    latest=True,
     _sudo=True,
 )
 
@@ -48,7 +63,8 @@ systemd.service(
     restarted=True,
 )
 
-# server.shell(
-#     name="Add VPS to tailnet and authorize using auth key",
-#     commands=[f"tailscale up --authkey={os.environ['TAILSCALE_AUTH_KEY']}"],
-# )
+server.shell(
+    name="Add VPS to tailnet and authorize using auth key",
+    commands=[f"tailscale up --authkey={os.environ['TAILSCALE_AUTH_KEY']}"],
+    _sudo=True
+)
