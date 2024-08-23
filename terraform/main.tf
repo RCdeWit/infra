1ef8c9f90c080fe7baa8554faa676496fe473c48
RCdeWit <RCdeWit@users.noreply.github.com>
2024-08-23 15:23:51 +0200

    Create remote Terraform backend on Tigris

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,20 @@
 terraform {
-  required_version = "1.5.5"
+  required_version = "1.9.5"
+
+  backend "s3" {
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    endpoints = {
+      s3 = "https://fly.storage.tigris.dev"
+    }
+    region = "auto"
+    bucket = "infra-tfstate"
+    key    = "terraform.tfstate"
+  }
 
   required_providers {
     digitalocean = {
