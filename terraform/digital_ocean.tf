66d4e2592748e832b5d735c1e27e29677e3d3456
RCdeWit <RCdeWit@users.noreply.github.com>
2024-08-23 14:06:56 +0200

    Update Ubuntu image for reverse proxy VPS

--- terraform/digital_ocean.tf
+++ terraform/digital_ocean.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "reverse_proxy_vps" {
   name     = "reverse-proxy-vps"
   size     = "s-1vcpu-1gb"
-  image    = "ubuntu-23-10-x64"
+  image    = "ubuntu-24-04-x64"
   region   = "ams3"
   ssh_keys = [var.digitalocean_ssh_key_id]
   tags     = var.default_tags
