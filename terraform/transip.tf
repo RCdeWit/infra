c774bfc5eadd9d2c8fa64956512e25f8e095c2f4
RCdeWit <RCdeWit@users.noreply.github.com>
2024-08-26 11:49:05 +0200

    Parse in nameservers from YAML

--- terraform/transip.tf
+++ terraform/transip.tf
@@ -12,23 +12,44 @@ locals {
     for domain in local.domains : [
       for record_type, records in domain.dns_records : [
         for record in records : {
-          domain  = domain.domain
-          name    = record.name
-          type    = record_type
+          domain = domain.domain
+          name   = record.name
+          type   = record_type
           # Ensure content is always a list
           content = flatten([record.value])
         }
       ]
     ]
   ])
+
+  # Flatten the nameservers records into a list
+  nameservers = {
+    for domain in local.domains : domain.domain => {
+      domain      = domain.domain
+      nameservers = domain.nameservers
+    }
+  }
 }
 
 # Iterate over each domain and create the transip_domain data sources
 # If this is a resource, Terraform will happily register any domains listed
 data "transip_domain" "domain" {
   for_each = { for domain in local.domains : domain.domain => domain }
+  name     = each.key
+}
 
-  name = each.key
+# Iterate over each domain and create the transip_domain data sources
+# If this is a resource, Terraform will happily register any domains listed
+resource "transip_domain_nameservers" "nameservers" {
+  for_each = local.nameservers
+  domain   = each.value.domain
+
+  dynamic "nameserver" {
+    for_each = each.value.nameservers
+    content {
+      hostname = nameserver.value
+    }
+  }
 }
 
 # Create DNS records for each domain
