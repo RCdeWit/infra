ccaea8da890bd27b937ccc1dfdc86ba5db3994e2
RCdeWit <RCdeWit@users.noreply.github.com>
2025-05-21 16:12:59 +0200

    Infer domain from env var

--- scripts/generate_caddyfile.py
+++ scripts/generate_caddyfile.py
@@ -4,26 +4,28 @@ import sys
 import yaml
 
 from pathlib import Path
-
 from utils.find_project_root import find_project_root
-
 
 PROJECT_ROOT = find_project_root()
 CONFIG_PATH = Path(f"{PROJECT_ROOT}/configs/services.yaml")
 OUTPUT_PATH = Path(f"{PROJECT_ROOT}/configs/generated/Caddyfile")
 UPSTREAM_IP = os.getenv("UPSTREAM_IP")
+DOMAIN_SUFFIX = os.getenv("TF_VAR_domain")
 
 def main():
-    upstream_ip = os.getenv("UPSTREAM_IP")
-    if not upstream_ip:
+    if not UPSTREAM_IP:
         print("Error: UPSTREAM_IP environment variable is not set", file=sys.stderr)
+        sys.exit(1)
+    if not DOMAIN_SUFFIX:
+        print("Error: TF_VAR_domain environment variable is not set", file=sys.stderr)
         sys.exit(1)
 
     config = yaml.safe_load(CONFIG_PATH.read_text())
     lines = []
-    for domain, port in config["services"].items():
+    for subdomain, port in config["services"].items():
+        domain = f"{subdomain}.{DOMAIN_SUFFIX}"
         lines.append(f"""{domain} {{
-    reverse_proxy {upstream_ip}:{port} {{
+    reverse_proxy {UPSTREAM_IP}:{port} {{
         header_up Host {{http.reverse_proxy.upstream.hostport}}
     }}
 }}\n""")
